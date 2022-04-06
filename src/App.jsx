b43816456be1ec5de8e79fb7aeb1db6d0bc8fc9c
Peter <peter@Peters-Air.lan>
2022-04-06 15:48:12 -0500

    splut api call to hope page and added routes to app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,18 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [villagers, setVillagers] = useState([
-    {
-      id: 1,
-      name: "Cyrano",
-      species: "Ant Eater",
-      image: "",
-    },
-  ]);
-
-  useEffect(() => {
-    let url = "https://acnhapi.com/v1a/villagers";
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.slice(0, 15));
-        console.log(data);
-        let newVillagers = data.map((villager) => ({
-          name: villager.name["name-USen"],
-          species: villager["species"],
-          id: villager.id,
-          image: villager.image_uri,
-        }));
-        console.log(newVillagers);
-        setVillagers(newVillagers);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  const villagerCards = villagers.map((villager) => (
-    <article key={villager.id}>
-      <img src={villager.image} alt={villager.name} />
-      <h2> {villager.name} </h2>
-      <p> {villager.species} </p>
-    </article>
-  ));
-
-  return <div className="App">{villagerCards}</div>;
+  return (
+    <div>
+      <Router>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
+    </div>
+  );
 }
 
 export default App;
