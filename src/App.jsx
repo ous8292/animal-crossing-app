ae13d5197831c809d262b961c9d8a5ffb4b62bf4
Peter <peter@Peters-Air.lan>
2022-04-06 13:21:15 -0500

    built api call that's grabbing all villagers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [villagers, setVillagers] = useState([
+    {
+      id: 1,
+      name: "Cyrano",
+      species: "Ant Eater",
+      image: "",
+    },
+  ]);
+
+  useEffect(() => {
+    let url = "https://acnhapi.com/v1a/villagers";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.slice(0, 15));
+        console.log(data);
+        let newVillagers = data.map((villager) => ({
+          name: villager.name["name-USen"],
+          species: villager["species"],
+          id: villager.id,
+          image: villager.image_uri,
+        }));
+        console.log(newVillagers);
+        setVillagers(newVillagers);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const villagerCards = villagers.map((villager) => (
+    <article key={villager.id}>
+      <img src={villager.image} alt={villager.name} />
+      <h2> {villager.name} </h2>
+      <p> {villager.species} </p>
+    </article>
+  ));
+
+  return <div className="App">{villagerCards}</div>;
 }
 
 export default App;
